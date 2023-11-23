af02cee621f4fec0c40842a3c6d9068c9f5e4398
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 17:49:02 +0100

    initial

---
+++ src/main/java/net/pullolo/magicabilities/MagicAbilities.java
@@ -0,0 +1,16 @@
+package net.pullolo.magicabilities;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MagicAbilities extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
